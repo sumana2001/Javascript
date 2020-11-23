388b17bc841d1d94f88a40c477fadaf2dc19027e
sumana2001 <sumana.basu2001@gmail.com>
2020-11-23 11:04:58 +0530

    Create guestlist.js

---
+++ guestlist.js
@@ -0,0 +1,8 @@
+var name=prompt("What is your name?");
+var guestlist=["gublu","buu","ghontu","golu","jhumi"];
+if(guestlist.includes(name)){
+    alert("Welcome "+ name);
+}
+else{
+    alert("Sorry! Please try next time.")
+}
