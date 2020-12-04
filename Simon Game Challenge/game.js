bbba6feaff83a4bae7fceb22cc97529d8bba680d
sumana2001 <sumana.basu2001@gmail.com>
2020-12-04 19:08:41 +0530

    Update game.js

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -1,7 +1,34 @@
+var userClickedPattern=[];
 var buttonColours=["red", "blue", "green", "yellow"];
 var gamePattern=[];
+
+$(".btn").on("click",function(event){
+    var userChosenColour=event.target.id;
+    userClickedPattern.push(userChosenColour);
+    playsound(userChosenColour);
+    animatePress(userChosenColour);
+   
+})
 function nextSequence(){
     var randomNumber=Math.floor(Math.random()*4);
     randomChosenColour=buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playsound(randomChosenColour);
 }
+
+function playsound(key){
+    var audio = new Audio("sounds/" + key + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+$(document).keypress(function(){
+    nextSequence();
+})
