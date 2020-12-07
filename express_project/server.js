797cece2107d2d06fa61cb1878c0f423f22f640e
sumana2001 <sumana.basu2001@gmail.com>
2020-12-07 12:48:58 +0530

    code

--- express_project/server.js
+++ express_project/server.js
@@ -1,5 +1,14 @@
 const express=require("express");
 const app=express();
+app.get("/",function(req,res){
+    res.send("Hello There!");
+});
+app.get("/about",function(req,res){
+    res.send("I am Sumana Basu!");
+});
+app.get("/contact",function(req,res){
+    res.send("Find me!");
+});
 app.listen(3000,function(){
     console.log("Server started on port 3000");
 })
