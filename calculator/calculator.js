18ea1b8db3006cd8c33bc9fe45a9e6ad5f2a481e
sumana2001 <sumana.basu2001@gmail.com>
2020-12-07 22:16:37 +0530

    bmi

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -15,6 +15,16 @@ app.post("/",function(req,res){
     res.send("The result is "+result);
 })
 
+app.get("/bmicalculator",function(req,res){
+    res.sendFile(__dirname+"/bmiCalculator.html");
+})
+
+app.post("/bmicalculator",function(req,res){
+    var w=Number(req.body.weight);
+    var h=Number(req.body.height);
+    var bmi = Math.floor(w/(h*h));
+    res.send("Your BMI is "+ bmi);
+})
 app.listen(3000,function(){
     console.log("Server started at port 3000");
 });
